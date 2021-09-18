6ffa16519ce95ec1cbeddb068853f41a309467da
Yoan Famel <90753356+yoan-famel@users.noreply.github.com>
2021-09-18 23:53:45 +0100

    Update speed-test.ps1

--- speed-test.ps1
+++ speed-test.ps1
@@ -1,12 +1,7 @@
 $ST_intro = @"
-                                                                                              
-      _/_/_/                                      _/    _/                            _/      
-   _/        _/_/_/      _/_/      _/_/      _/_/_/  _/_/_/_/    _/_/      _/_/_/  _/_/_/_/   
-    _/_/    _/    _/  _/_/_/_/  _/_/_/_/  _/    _/    _/      _/_/_/_/  _/_/        _/        
-       _/  _/    _/  _/        _/        _/    _/    _/      _/            _/_/    _/         
-_/_/_/    _/_/_/      _/_/_/    _/_/_/    _/_/_/      _/_/    _/_/_/  _/_/_/        _/_/      
-         _/                                                                                   
-        _/                                                                                    
+ __   ___   ____  ____  ___      _____  ____  __  _____ 
+( (` | |_) | |_  | |_  | | \      | |  | |_  ( (`  | |  
+_)_) |_|   |_|__ |_|__ |_|_/      |_|  |_|__ _)_)  |_|                                                                                     
 "@
 Write-Host $ST_Intro -ForegroundColor DarkCyan
 Write-Host '====================================================================================' -ForegroundColor DarkCyan
@@ -15,58 +10,57 @@ Write-Host 'Enter a PC below' -ForegroundColor DarkCyan
 Write-Host ''
 
 while ($true) {
-$installer = "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-win64.zip"
+	$installer = "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-win64.zip"
+	$hostname = Read-Host "PC"
 
-$hostname = Read-Host "PC"
+	ForEach ($i in $hostname) {
 
-ForEach ($i in $hostname) {
-
-	$testConnection = Test-Connection -Count 2 $hostname -ErrorAction 'silentlycontinue'
+		$testConnection = Test-Connection -Count 2 $hostname -ErrorAction 'silentlycontinue'
 	
-	If ($testConnection -ne $null) {
+		If ($testConnection -ne $null) {
 		
-		$remoteDLpath = "\\$hostname\c$\SpeedTest.Zip"
-		$extraction = "\\$hostname\c$\SpeedTest"
-		$EXE = "\\$hostname\c$\SpeedTest\speedtest.exe"
+			$remoteDLpath = "\\$hostname\c$\SpeedTest.Zip"
+			$extraction = "\\$hostname\c$\SpeedTest"
+			$EXE = "\\$hostname\c$\SpeedTest\speedtest.exe"
 		
-		function RunTest() {
-			$test = & $EXE --accept-license --accept-gdpr #& allow run of command stored in var
-			$test
-		}
-		
-		#Check if it leads to a file
-		if (Test-Path -Path $EXE -PathType leaf) {
-			
-			Write-Host "Installer is already installed. [RUNNING SPEEDTEST...]" -ForegroundColor Green
-			RunTest
-			
-		} else {
-			
-			Write-Host "Installer doesn't exist. [DOWNLOADING FILE...]" -ForegroundColor Yellow
-			wget $installer -outfile $remoteDLpath
-			
-			
-			Add-Type -AssemblyName System.IO.Compression.FileSystem
-			function Unzip {
-				param([string]$zipfile, [string]$outpath)
-				[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+			function RunTest() {
+				$test = & $EXE --accept-license --accept-gdpr #& allow run of command stored in var
+				$test
 			}
-			
-			Unzip $remoteDLpath $extraction
-			
-			Write-Host "Download complete. [RUNNING SPEEDTEST...]" -ForegroundColor Green
-			RunTest
-			
-			Remove-Item -Path $remoteDLpath -recurse
-			
-		}
 		
-	} Else {
+			#Check if it leads to a file
+            if (Test-Path -Path $EXE -PathType leaf) {
+                
+                Write-Host "Installer is already installed. [RUNNING SPEEDTEST...]" -ForegroundColor Green
+                RunTest
+                
+            } else {
+                
+                Write-Host "Installer doesn't exist. [DOWNLOADING FILE...]" -ForegroundColor Yellow
+                wget $installer -outfile $remoteDLpath
+                
+                Add-Type -AssemblyName System.IO.Compression.FileSystem
+                function Unzip {
+					
+                    param([string]$zipfile, [string]$outpath)
+                    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+                }
+                
+                Unzip $remoteDLpath $extraction
+                
+                Write-Host "Download complete. [RUNNING SPEEDTEST...]" -ForegroundColor Green
+                RunTest
+                
+                Remove-Item -Path $remoteDLpath -recurse
+                
+            }
 		
-		Write-Host "$hostname IS OFFLINE" -ForegroundColor Red
+	    } Else {
 		
-	}
-}
-
-Read-Host "Press CTRL + C to quit or press ENTER to continue"
+		    Write-Host "$hostname IS OFFLINE" -ForegroundColor Red
+		
+	    }
+    }
+    
+    Read-Host "Press CTRL + C to quit or press ENTER to continue"
 }
