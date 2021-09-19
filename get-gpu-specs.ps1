939fa3cece9c6b55665f1fd729d7fc650e32cbf0
Yoan Famel <90753356+yoan-famel@users.noreply.github.com>
2021-09-19 01:25:37 +0100

    Add files via upload

---
+++ get-gpu-specs.ps1
@@ -0,0 +1,21 @@
+$i = Read-Host "PC"
+ForEach ($i in $pc) {	
+
+	$testCo = Test-Connection -BufferSize 32 -Count 1 -ComputerName $i -Quiet
+		
+	If ($testCo -eq $true) {
+	
+		Write-Host "ONLINE" -ForegroundColor Green
+
+		ForEach ($Server in $i) { 
+			
+			Invoke-Command -ComputerName $i -ScriptBlock {
+				$displayAdaptors = get-wmiobject -ComputerName $i win32_VideoController | select caption
+				$i, $displayAdaptors | ft -HideTableHeaders
+			}
+		}
+	}  Else {
+		
+		Write-Host "OFFLINE" -ForegroundColor Red
+	}	
+}
