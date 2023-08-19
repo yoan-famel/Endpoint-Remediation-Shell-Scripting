7d0c088df8fff6a579feef04f77590cb47b36b7c
Yoan Famel <90753356+yoan-famel@users.noreply.github.com>
2023-08-19 19:04:40 +0100

    Create patch-cisco-anyconnect-client.ps1

---
+++ patch-cisco-anyconnect-client.ps1
@@ -0,0 +1,4 @@
+#https://www.powershellgallery.com/packages/Get-ProgramUninstallString/1.0/Content/Get-ProgramUninstallString.ps1
+
+cd 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\'
+.\Uninstall.exe -remove -silent
